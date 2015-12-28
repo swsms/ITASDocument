9db44e471cc3957d39f641db4032fd5dfc544f79
akdeniso <akdeniso@gmail.com>
2015-12-29 02:05:00 +0500

    fix #3

--- src/main/java/server/DAO/UserDAO.java
+++ src/main/java/server/DAO/UserDAO.java
@@ -4,31 +4,35 @@ import org.hibernate.Criteria;
 import org.hibernate.HibernateException;
 import org.hibernate.Session;
 import org.hibernate.criterion.Restrictions;
+
+import server.PasswordEncoder;
 import server.entities.UserEntity;
 
 public class UserDAO {
 
-    private Session session;
+	private Session session;
 
-    public UserDAO(Session session) {
-        this.session = session;
-    }
+	public UserDAO(Session session) {
+		this.session = session;
+	}
 
-    public UserEntity get(long id) throws HibernateException {
-        return (UserEntity) session.get(UserEntity.class, id);
-    }
+	public UserEntity get(long id) throws HibernateException {
+		return (UserEntity) session.get(UserEntity.class, id);
+	}
 
-    public UserEntity get(String login, String password) throws HibernateException {
-        Criteria criteria = session.createCriteria(UserEntity.class);
-        return ((UserEntity) criteria
-                .add(Restrictions.eq("login", login))
-                .add(Restrictions.eq("password", password))
-                .uniqueResult());
-    }
+	public UserEntity get(String login, String password)
+			throws HibernateException {
+		Criteria criteria = session.createCriteria(UserEntity.class);
+		return ((UserEntity) criteria
+				.add(Restrictions.eq("login", login))
+				.add(Restrictions.eq("password",
+						PasswordEncoder.encode(password))).uniqueResult());
+	}
 
-    public long insertUser(String name, String login, String password) throws HibernateException {
-        UserEntity userEntity = new UserEntity(name, login, password);
-        return (Long) session.save(userEntity);
-    }
-
+	public long insertUser(String name, String login, String password)
+			throws HibernateException {
+		UserEntity userEntity = new UserEntity(name, login,
+				PasswordEncoder.encode(password));
+		return (Long) session.save(userEntity);
+	}
 }
