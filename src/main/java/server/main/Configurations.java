d1f159f851147b3cd8e4b888ca6b542e6cef1851
akdeniso <akdeniso@gmail.com>
2015-12-29 01:17:53 +0500

    resolve #2

--- src/main/java/server/main/Configurations.java
+++ src/main/java/server/main/Configurations.java
@@ -1,47 +1,58 @@
 package server.main;
 
 import org.hibernate.cfg.Configuration;
+
+import server.entities.RoleEntity;
 import server.entities.UserEntity;
 
 public class Configurations {
-    private static final String hibernate_show_sql = "true";
-    private static final String hibernate_hbm2ddl_auto = "validate";
+	private static final String hibernate_show_sql = "true";
+	private static final String hibernate_hbm2ddl_auto = "validate";
 
-    public static Configuration getPostGresConfigurationRemote() {
-        Configuration configuration = getBaseConfiguration();
+	public static Configuration getPostGresConfigurationRemote() {
+		Configuration configuration = getBaseConfiguration();
 
-        configuration.setProperty("hibernate.connection.driver_class", "org.postgresql.Driver");
-        configuration.setProperty("hibernate.connection.url",
-                "jdbc:postgresql://ec2-54-195-252-202.eu-west-1.compute.amazonaws.com:5432/da7cn3tiopjdff?" +
-                        "ssl=true&sslfactory=org.postgresql.ssl.NonValidatingFactory");
-        configuration.setProperty("hibernate.connection.username", "jgneczrdeubazg");
-        configuration.setProperty("hibernate.connection.password", "6CpgQ4Teo4bnlICe3OzCZu4iOB");
-        configuration.setProperty("hibernate.show_sql", hibernate_show_sql);
-        configuration.setProperty("hibernate.hbm2ddl.auto", hibernate_hbm2ddl_auto);
+		configuration.setProperty("hibernate.connection.driver_class",
+				"org.postgresql.Driver");
+		configuration
+				.setProperty(
+						"hibernate.connection.url",
+						"jdbc:postgresql://ec2-54-195-252-202.eu-west-1.compute.amazonaws.com:5432/da7cn3tiopjdff?"
+								+ "ssl=true&sslfactory=org.postgresql.ssl.NonValidatingFactory");
+		configuration.setProperty("hibernate.connection.username",
+				"jgneczrdeubazg");
+		configuration.setProperty("hibernate.connection.password",
+				"6CpgQ4Teo4bnlICe3OzCZu4iOB");
+		configuration.setProperty("hibernate.show_sql", hibernate_show_sql);
+		configuration.setProperty("hibernate.hbm2ddl.auto",
+				hibernate_hbm2ddl_auto);
 
-        return configuration;
-    }
+		return configuration;
+	}
 
-    public static Configuration getPostGresConfigurationLocal() {
-        Configuration configuration = getBaseConfiguration();
+	public static Configuration getPostGresConfigurationLocal() {
+		Configuration configuration = getBaseConfiguration();
 
-        configuration.setProperty("hibernate.connection.driver_class", "org.postgresql.Driver");
-        configuration.setProperty("hibernate.connection.url", "jdbc:postgresql://localhost:5432/postgres");
-        configuration.setProperty("hibernate.connection.username", "postgres");
-        configuration.setProperty("hibernate.connection.password", "q");
-        configuration.setProperty("hibernate.show_sql", hibernate_show_sql);
-        configuration.setProperty("hibernate.hbm2ddl.auto", hibernate_hbm2ddl_auto);
+		configuration.setProperty("hibernate.connection.driver_class",
+				"org.postgresql.Driver");
+		configuration.setProperty("hibernate.connection.url",
+				"jdbc:postgresql://localhost:5432/postgres");
+		configuration.setProperty("hibernate.connection.username", "postgres");
+		configuration.setProperty("hibernate.connection.password", "q");
+		configuration.setProperty("hibernate.show_sql", hibernate_show_sql);
+		configuration.setProperty("hibernate.hbm2ddl.auto",
+				hibernate_hbm2ddl_auto);
 
-        return configuration;
-    }
+		return configuration;
+	}
 
-    private static Configuration getBaseConfiguration() {
-        Configuration configuration = new Configuration();
+	private static Configuration getBaseConfiguration() {
+		Configuration configuration = new Configuration();
 
-        configuration.addAnnotatedClass(UserEntity.class);
-        //configuration.addAnnotatedClass(RoleEntity.class);
+		configuration.addAnnotatedClass(UserEntity.class);
+		configuration.addAnnotatedClass(RoleEntity.class);
 
-        return configuration;
-    }
+		return configuration;
+	}
 
 }
