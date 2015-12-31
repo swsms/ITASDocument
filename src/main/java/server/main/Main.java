63c374354c917201639f05ceef716607ae2ebbb3
Artyom <swsms@mail.ru>
2015-12-31 05:21:58 +0500

    Модный GUI

--- src/main/java/server/main/Main.java
+++ src/main/java/server/main/Main.java
@@ -28,7 +28,7 @@ public class Main {
 	public static void main(String[] args) throws Exception {
 
 		Configuration configuration = Configurations
-				.getPostGresConfigurationRemote();
+				.getPostGresConfigurationLocal();
 		SessionFactory sessionFactory = Configurations
 				.createSessionFactory(configuration);
 
